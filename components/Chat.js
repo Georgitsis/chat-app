c430b622632d2aa7b24687d7aa09b6c48008b459
PsychoBoyJAck83 <georgitsis.theodoros@gmail.com>
2023-08-17 22:41:10 +0200

    1st commit

---
+++ components/Chat.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+
+const Chat = () => {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  );
+};
